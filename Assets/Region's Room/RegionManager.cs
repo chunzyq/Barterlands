eca8684b5352bdc4625addbd4d6f24a92a8eff75
chunzyq <denis2003201320@gmail.com>
2025-05-30 18:03:20 +0300

    add new icons for buildings

--- Assets/Region's Room/RegionManager.cs
+++ Assets/Region's Room/RegionManager.cs
@@ -5,6 +5,7 @@ using Zenject;
 public class RegionManager : MonoBehaviour
 {
     [SerializeField] private List<GameRegion> allRegions = new List<GameRegion>();
+    [Inject] SettlementManager settlementManager;
 
     void Start()
     {
@@ -12,22 +13,34 @@ public class RegionManager : MonoBehaviour
         {
             region.OnStateChanged += OnRegionsStateChanged;
         }
+        settlementManager.OnSettlementLevelChanged += ChangeLevel;
     }
-
+    void OnDestroy()
+    {
+        settlementManager.OnSettlementLevelChanged -= ChangeLevel;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            ChangeLevel();
+        }
+    }
     private void OnRegionsStateChanged(GameRegion region, RegionState newState)
     {
         Debug.Log($"Регион {region.regionData.coordinates} изменил состояние на {newState}");
     }
 
-    void Update()
-    {
+    // void Update()
+    // {
 
-        if (Input.GetKeyDown(KeyCode.B))
-        {
-            foreach (GameRegion region in allRegions)
-            {
-                region.UnlockRegion(region);
-            }
-        }
+    //     if (Input.GetKeyDown(KeyCode.B))
+    //     {
+    //         settlementManager.LevelUp(SettlementLevel.MakeShiftShelter);
+    //     }
+    // }
+    void ChangeLevel()
+    {
+        settlementManager.TierUp(SettlementLevel.MakeShiftShelter);
     }
 }
--- Assets/Settlement/SettlementManager.cs
+++ Assets/Settlement/SettlementManager.cs
@@ -13,6 +13,7 @@ public class SettlementManager : MonoBehaviour
 
     [Header("Управление")]
     [SerializeField] private List<BuildingInstance> buildingList = new List<BuildingInstance>();
+    [SerializeField] private SettlementLevel _currentLevel = SettlementLevel.RefugeeCamp;
 
     public int totalPopulation { get; private set; }
     public int TotalAssignedWorkers => buildingList.Sum(b => b.CurrentWorkerCount);
@@ -21,7 +22,7 @@ public class SettlementManager : MonoBehaviour
     public event Action<int> OnPopulationChange;
     public event Action<BuildingInstance> OnBuildingRegistered;
     public event Action<BuildingInstance> OnBuildingUnregistered;
-
+    public event Action OnSettlementLevelChanged;
     void Awake()
     {
         totalPopulation = startPeopleCount;
@@ -32,7 +33,7 @@ public class SettlementManager : MonoBehaviour
         if (!buildingList.Contains(building))
         {
             buildingList.Add(building);
-            
+
             OnBuildingRegistered?.Invoke(building);
         }
     }
@@ -55,7 +56,7 @@ public class SettlementManager : MonoBehaviour
         return buildingList.FirstOrDefault(b => b.InstanceID == id);
     }
 
-     public List<BuildingInstance> GetBuildingsByType(BuildingType type)
+    public List<BuildingInstance> GetBuildingsByType(BuildingType type)
     {
         return buildingList.Where(b => b.buildingData.buildingType == type).ToList();
     }
@@ -68,7 +69,7 @@ public class SettlementManager : MonoBehaviour
     public bool AssignWorkers(BuildingInstance building, int amount)
     {
         if (!CanAssignWorkers(amount)) return false;
-        
+
         // Логика назначения рабочих
         switch (building.buildingData.buildingType)
         {
@@ -79,7 +80,7 @@ public class SettlementManager : MonoBehaviour
                 building.laboratorySettings.currentLaboratoryWorkers += amount;
                 break;
         }
-        
+
         return true;
     }
 
@@ -89,11 +90,11 @@ public class SettlementManager : MonoBehaviour
         switch (building.buildingData.buildingType)
         {
             case BuildingType.Factory:
-                building.factorySettings.currentFactoryWorkers = 
+                building.factorySettings.currentFactoryWorkers =
                     Mathf.Max(0, building.factorySettings.currentFactoryWorkers - amount);
                 break;
             case BuildingType.Laboratory:
-                building.laboratorySettings.currentLaboratoryWorkers = 
+                building.laboratorySettings.currentLaboratoryWorkers =
                     Mathf.Max(0, building.laboratorySettings.currentLaboratoryWorkers - amount);
                 break;
         }
@@ -110,128 +111,13 @@ public class SettlementManager : MonoBehaviour
     {
         var factories = GetBuildingsByType(BuildingType.Factory);
         if (factories.Count == 0) return 100f;
-        
+
         return factories.Average(f => f.factorySettings.currentFacEfficiency);
     }
 
-    // public Dictionary<ResourceType, int> resourceStock;
-    // public IReadOnlyDictionary<ResourceType, int> ResourceStock => resourceStock;
-    // public List<BuildingInstance> buildingList = new List<BuildingInstance>();
-    // public event Action<ResourceType, int> OnResourceChange;
-
-    // [SerializeField] private float productionInterval = 15f;
-
-
-    // void Awake()
-    // {
-    //     resourceStock = new Dictionary<ResourceType, int>();
-    //     foreach (ResourceType rt in System.Enum.GetValues(typeof(ResourceType)))
-    //     {
-    //         resourceStock[rt] = 0;
-    //     }
-    // }
-    // private void Start()
-    // {
-    //     StartCoroutine(ProduceResources());
-    // }
-    // private IEnumerator ProduceResources()
-    // {
-    //     while (true)
-    //     {
-    //         yield return new WaitForSeconds(productionInterval);
-
-    //         var production = GetTotalHourlyProduction();
-    //         foreach (var kv in production)
-    //         {
-    //             resourceStock[kv.Key] += kv.Value;
-    //             OnResourceChange?.Invoke(kv.Key, resourceStock[kv.Key]);
-    //         }
-    //     }
-    // }
-    // public void RegisterBuilding(BuildingInstance building)
-    // {
-    //     if (!buildingList.Contains(building))
-    //     {
-    //         buildingList.Add(building);
-    //     }
-    // }
-    // public void UnregisterBuilding(BuildingInstance building)
-    // {
-    //     buildingList.Remove(building);
-    // }
-
-    // public int TotalAssignedWorkers => buildingList.Sum(b => b.CurrentWorkerCount);
-    // public int FreeWorkers => startPeopleCount - TotalAssignedWorkers;
-
-    // public bool CanAfford(Dictionary<ResourceType, int> cost)
-    // {
-    //     return cost.All(kv => resourceStock[kv.Key] >= kv.Value);
-
-    //     // foreach (var kv in cost)
-    //     // {
-    //     //     if (resourceStock[kv.Key] < kv.Value)
-    //     //     {
-    //     //         return false;
-    //     //     }
-    //     // }
-    //     // return true;
-    // }
-
-    // public bool SpendResources(Dictionary<ResourceType, int> cost)
-    // {
-    //     if (!CanAfford(cost)) return false;
-    //     foreach (var kv in cost)
-    //     {
-    //         resourceStock[kv.Key] -= kv.Value;
-    //         OnResourceChange?.Invoke(kv.Key, resourceStock[kv.Key]);
-    //     }
-    //     return true;
-    // }
-    // public void AddResources(ResourceType type, int amount)
-    // {
-    //     resourceStock[type] += amount;
-    //     OnResourceChange?.Invoke(type, resourceStock[type]);
-    // }
-    // public Dictionary<ResourceType, int> GetTotalHourlyProduction()
-    // {
-    //     var total = new Dictionary<ResourceType, int>();
-    //     foreach (var b in buildingList)
-    //     {
-    //         var single = b.GetHourlyProduction();
-    //         foreach (var kv in single)
-    //         {
-    //             if (!total.ContainsKey(kv.Key))
-    //             {
-    //                 total[kv.Key] = 0;
-    //             }
-    //             total[kv.Key] += kv.Value;
-    //         }
-    //     }
-    //     return total;
-    // }
-
-    // public BuildingInstance TryBuild(BuildingData data, Vector3 pos)
-    // {
-    //     var cost = data.cost.ToDictionary(c => c.type, c => c.amount);
-    //     if (!SpendResources(cost))
-    //     {
-    //         return null;
-    //     }
-    //     var go = Instantiate(data.buildingPrefab, pos, Quaternion.identity);
-    //     var instance = go.GetComponent<BuildingInstance>();
-
-    //     instance.GenerateUniqueID();
-    //     RegisterBuilding(instance);
-    //     return instance;
-    // }
-
-    // public BuildingInstance GetBuildingByID(string id)
-    // {
-    //     return buildingList.FirstOrDefault(b => b.InstanceID == id);
-    // }
-    // public void NotifyResourceChange(ResourceType type, int amount)
-    // {
-    //     OnResourceChange?.Invoke(type, amount);
-    // }
-
+    public void TierUp(SettlementLevel newTier)
+    {
+        _currentLevel = newTier;
+        Debug.Log($"Уровень поселения повышен до: {newTier}");
+    }
 }
